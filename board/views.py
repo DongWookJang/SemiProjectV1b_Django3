dcb0414de170bbe3f39b3b8ea623bf65c13a002c
bigdata <jdw1067@gmail.com>
2022-06-21 12:37:45 +0900

    Django3 app urls.py views.py 수정 - home, member, board

--- board/views.py
+++ board/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+def list(request):
+    return render(request, 'board/list.html')
+#                          파일명
+
+
+def views(request):
+    return render(request, 'board/views.html')
+
+
+def write(request):
+    return render(request, 'board/write.html')
